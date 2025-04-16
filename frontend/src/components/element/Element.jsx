b36832aa8fbc100ac68e0df526f53d003b9f31c9
midhunk0 <midhunrajk009@gmail.com>
2025-04-16 15:08:56 +0530

    modified ui

--- frontend/src/components/element/Element.jsx
+++ frontend/src/components/element/Element.jsx
@@ -18,6 +18,27 @@ export default function Element() {
         return ()=>window.removeEventListener("resize", handleResize);
     }, []);
 
+    // const unique=[...new Set(data.elements.map(element=>element["cpk-hex"]))];
+    // console.log(unique);
+
+    function renderElectronConfig(config) {
+        const parts = config.split(' ').map((part, index) => {
+            const match = part.match(/^(\d)([spdf])(\d+)$/);
+            if (match) {
+                const [, n, l, p] = match;
+                return (
+                    <span key={index}>
+                        {n}{l}<sup>{p}</sup>&nbsp;
+                    </span>
+                );
+            }
+            return <span key={index}>{part}&nbsp;</span>;
+        });
+    
+        return <span className="box">{parts}</span>;
+    }
+    
+
     return (
         <div className="element">
             <div className="element-header">
@@ -27,7 +48,7 @@ export default function Element() {
                         {width<600 ? prevElement.symbol : prevElement.name}
                     </button>
                 : <div/>}
-                <p className="element-name">{element.name} ({element.symbol})</p>
+                <h1 className="element-name">{element.name} ({element.symbol})</h1>
                 {nextElement ? 
                     <button onClick={()=>navigate(`/element/${number+1}`)}>
                         {width<600 ? nextElement.symbol : nextElement.name}
@@ -35,59 +56,129 @@ export default function Element() {
                     </button>
                 : <div/>}
             </div>
-        <div className="element-container">
-            <model-viewer
-                alt={element.name}
-                src={element.bohr_model_3d}
-                camera-controls
-                ar
-                auto-rotate
-                shadow-intensity="1"
-                exposure="1"
-            ></model-viewer>
+            
+            <div className="element-container">
+                <model-viewer
+                    alt={element.name}
+                    src={element.bohr_model_3d}
+                    camera-controls
+                    ar
+                    auto-rotate
+                    shadow-intensity="1"
+                    exposure="1"
+                ></model-viewer>
 
-            <div className="element-info-panel">
-                <div className="element-sections">
-                    <div className="element-info-card">
-                        <h2>General</h2>
-                        <p>Number: {element.number}</p>
-                        <p>Category: {element.category}</p>
-                        <p>Phase: {element.phase}</p>
-                        <p>Block: {element.block}</p>
+                <div className="element-info-panel">
+                    <div className="element-first-row">
+
+                    <div className="element-card">
+                        <div className="element-card-row">
+                            <p>{element.number}</p>
+                            <p>{element.phase}</p>
+                        </div>
+                        <div className="element-card-row1">
+                            <p className="symbol">{element.symbol}</p>
+                            <p>{element.name}</p>
+                        </div>
+                        <div className="element-card-row">
+                            <p className="category">{element.category}</p>
+                            <p className={`block ${element.block}`}>{element.block}</p>
+                        </div>
                     </div>
-                    <div className="element-info-card">
-                        <h2>Discovery</h2>
-                        <p>Discovered by: {element.discovered_by}</p>
-                        <p>Named by: {element.named_by}</p>
-                        <a href={element.source} target="_blank" rel="noreferrer">Wiki</a>
+                    <div className="element-image-section">
+                        <img src={element.image.url} alt={element.name} />
+                        <p>{element.image.title}</p>
                     </div>
-                    <div className="element-info-card">
-                        <h2>Structure</h2>
-                        <p>Atomic Mass: {element.atomic_mass}</p>
-                        <p>Boiling Point: {element.boil}</p>
-                        <p>Melting Point: {element.melt}</p>
-                        <p>Density: {element.density}</p>
-                        <p>Molar Heat: {element.molar_heat}</p>
+                    </div>
+
+                    <div className="element-summary">
+                        <h2>Details</h2>
+                        <p>{element.summary}</p>
                     </div>
                     <div className="element-info-card">
                         <h2>Electron Details</h2>
-                        <p>Electron Affinity: {element.electron_affinity}</p>
-                        <p>Electronegativity: {element.electronegativity_pauling}</p>
-                        <p>Shells: {element.shells?.join(", ")}</p>
-                        <p>Electron Config: {element.electron_configuration}</p>
-                        <p>Semantic Config: {element.electron_configuration_semantic}</p>
+                        {element.shells && 
+                            <div className="element-card-div">
+                                <p>Shell Config</p>
+                                <span className="box">{element.shells?.join(", ")}</span>
+                            </div>
+                        }
+                        {element.electron_configuration && 
+                            <div className="element-card-div">
+                                <p>Electron Config</p>
+                                {renderElectronConfig(element.electron_configuration)}
+                            </div>
+                        }
+                        {element.electron_configuration_semantic && 
+                            <div className="element-card-div">
+                                <p>Semantic Config</p>
+                                {renderElectronConfig(element.electron_configuration_semantic)}
+                            </div>
+                        }
+                        {element.electronegativity_pauling && 
+                            <div className="element-card-div">
+                                <p>Electronegativity</p>
+                                <span className="box">{element.electronegativity_pauling}</span>
+                            </div>
+                        }
+                        {element.electron_affinity && 
+                            <div className="element-card-div">
+                                <p>Electron Affinity</p>
+                                <span className="box">{element.electron_affinity}</span>
+                            </div>
+                        }
+                    </div>
+                    <div className="element-info-card">
+                        <h2>Discovery</h2>
+                        {element.discovered_by && 
+                            <div className="element-card-div">
+                                <p>Discovered by</p>
+                                <span className="box">{element.discovered_by}</span>
+                            </div>
+                        }
+                        {element.named_by && 
+                            <div className="element-card-div">
+                                <p>Named by</p>
+                                <span className="box">{element.named_by}</span>
+                            </div>
+                        }
+                        {element.source && <a href={element.source} target="_blank" rel="noreferrer">More Info</a>}
+                    </div>
+                    <div className="element-info-card">
+                        <h2>Structure</h2>
+                        {element.atomic_mass && 
+                            <div className="element-card-div">
+                                <p>Atomic Mass</p>
+                                <span className="box">{element.atomic_mass}</span>
+                            </div>
+                        }
+                        {element.boil && 
+                            <div className="element-card-div">
+                                <p>Boiling Point</p>
+                                <span className="box">{element.boil}</span>
+                            </div>
+                        }
+                        {element.melt && 
+                            <div className="element-card-div">
+                                <p>Melting Point</p>
+                                <span className="box">{element.melt}</span>
+                            </div>
+                        }
+                        {element.density && 
+                            <div className="element-card-div">
+                                <p>Density</p>
+                                <span className="box">{element.density}</span>
+                            </div>
+                        }
+                        {element.molar_heat && 
+                            <div className="element-card-div">
+                                <p>Molar Heat</p>
+                                <span className="box">{element.molar_heat}</span>
+                            </div>
+                        }
                     </div>
                 </div>
-                <div className="element-summary">
-                    <h2>Summary</h2>
-                    <p>{element.summary}</p>
-                </div>
-                <div className="element-image-section">
-                    <h2>{element.image.title}</h2>
-                    <img src={element.image.url} alt={element.name} />
-                </div>
             </div>
-        </div>
         </div>
     );
 }
